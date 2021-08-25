4b6fec635831d2075b8b5d12a192bf1b7597a4ff
afonsofrancof <afonsofrancof@gmail.com>
2021-08-25 19:35:19 +0100

    Started Implementation of Profile Page

--- app/src/main/java/com/afonsofrancof/connectdot/fragments/ProfileFragment.kt
+++ app/src/main/java/com/afonsofrancof/connectdot/fragments/ProfileFragment.kt
@@ -1,23 +1,79 @@
 package com.afonsofrancof.connectdot.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.afonsofrancof.connectdot.DividerItemDecoration
+import com.afonsofrancof.connectdot.FeedAdapter
+import com.afonsofrancof.connectdot.MainActivity
 import com.afonsofrancof.connectdot.databinding.FragmentFeedBinding
 import com.afonsofrancof.connectdot.databinding.FragmentProfileBinding
+import com.afonsofrancof.connectdot.objects.Post
+import com.afonsofrancof.connectdot.objects.User
+import com.afonsofrancof.connectdot.utils.getUser
+import com.afonsofrancof.connectdot.viewModels.FeedViewModel
 
-class ProfileFragment: Fragment() {
+class ProfileFragment: Fragment(),FeedAdapter.OnClickListener {
+
+    private val viewModel: FeedViewModel by lazy {
+        ViewModelProvider(this).get(FeedViewModel::class.java)
+    }
+
+    lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentProfileBinding.inflate(inflater)
+        binding = FragmentProfileBinding.inflate(inflater)
+
+        val adapter = FeedAdapter(this)
+        binding.feed.addItemDecoration(DividerItemDecoration(30))
+        binding.feed.adapter = adapter
+        binding.feed.itemAnimator = FeedFragment.Animator()
+        viewModel.getPosts()
+
+        viewModel.postList.observe(viewLifecycleOwner, Observer {
+            //Falta FILTRAR POR ESTE USER SO
+            adapter.submitList(it)
+            Log.i("debug",
+                (binding.feed.layoutManager as GridLayoutManager).findFirstCompletelyVisibleItemPosition()
+                    .toString()
+            )
+            if ((binding.feed.layoutManager as GridLayoutManager).findFirstCompletelyVisibleItemPosition() < 2) {
+                binding.feed.smoothScrollToPosition(0)
+            }
+        })
 
 
 
         return binding.root
+    }
+
+    override fun onClickLike(post: Post) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickRepost(post: Post) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickUser(author: User) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickImage(post: Post) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickDelete(post: Post) {
+        TODO("Not yet implemented")
     }
 }
