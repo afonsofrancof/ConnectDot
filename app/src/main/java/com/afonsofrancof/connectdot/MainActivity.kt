5a14ee557c7b0a80677025dfdaa208174925b73a
afonsofrancof <afonsofrancof@gmail.com>
2021-08-31 12:29:19 +0100

    Added Image support on chat and changed chat bubble drawable

--- app/src/main/java/com/afonsofrancof/connectdot/MainActivity.kt
+++ app/src/main/java/com/afonsofrancof/connectdot/MainActivity.kt
@@ -11,55 +11,70 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.afonsofrancof.connectdot.databinding.ActivityMainBinding
+import com.afonsofrancof.connectdot.objects.Chat
+import com.afonsofrancof.connectdot.utils.getUser
 import com.google.firebase.auth.FirebaseAuth
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         navController = this.findNavController(R.id.host_fragment)
-        NavigationUI.setupWithNavController(binding.bottomNavigation,navController)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
-            binding.bottomNavigation.isVisible = destination.id!=R.id.postCreateFragment
+            binding.bottomNavigation.isVisible = destination.id != R.id.postCreateFragment
+            when (destination.id) {
+                R.id.chatAddListFragment -> title = "Add a new chat"
+                R.id.chatWindowFragment -> if (arguments != null) {
+                    title =
+                        (arguments.getParcelable<Chat>("chat"))?.userList?.first { it.userId != getUser().userId }?.name
+                            ?: ""
+                }
+                R.id.ChatPickerListFragment -> title = "Pick a chat"
+                R.id.profileFragment -> title = "Profile"
+                R.id.feedFragment -> title = "Feed"
+                R.id.settingsFragment -> title = "Settings"
+                R.id.postCreateFragment -> title = "Create new post"
+            }
         }
 
 
-
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.settings,menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.settings, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        navController.navigate(R.id.settingsFragment)
+//        return super.onOptionsItemSelected(item)
+//    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        navController.navigate(R.id.settingsFragment)
-        return super.onOptionsItemSelected(item)
-    }
-
-    fun newPost(){
+    fun newPost() {
         navController.navigate(R.id.postCreateFragment)
     }
 
-    fun newChat(){
+    fun newChat() {
         navController.navigate(R.id.chatAddListFragment)
     }
-    
+
 //    fun goToChat(){
 //        navController.navigate(R.id.chat)
 //    }
 
-    fun goBack(){
+    fun goBack() {
         navController.popBackStack()
     }
 
-    fun signOut(){
+    fun signOut() {
         FirebaseAuth.getInstance().signOut()
         startActivity(Intent(Intent(this, LoginActivity::class.java)))
         finish()
